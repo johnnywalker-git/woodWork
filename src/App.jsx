08a74896ed0b87ac08c7f9ebb93c0d15e562e718
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2026-07-09 23:51:28 +0100

    Scroll issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -7,11 +7,22 @@ import Work from './pages/Work';
 import ServicesPage from './pages/ServicesPage';
 import Process from './pages/Process';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
       <div className="relative bg-deep-walnut min-h-screen">
         <div className="noise-overlay"></div>
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
